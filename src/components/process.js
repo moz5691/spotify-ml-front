ed44d8c91bd0bec98357546b4ef1c7a0bc4e63b5
C. Ahn <moz5691@gmail.com>
2020-08-04 20:23:13 -0400

    process document markdown

--- src/components/process.js
+++ src/components/process.js
@@ -1,51 +1,54 @@
 const process = `
-## Process and Conclusion
+## Following Typical ML pipeline process
 
-### Following Typical ML pipeline
+![](./docImages/ml_pipeline_01.png)
 
-![](/docImages/ml_pipeline_01.png)
+## 1. Collect data set
 
-### 1. Collect data set
+    Started with 170K songs from Spotify.
 
-Started with 170K songs from Spotify.
+## 2. Feature engineering
 
-### 2. Feature engineering
+    Clean the dataset, convert string to numeric, filling null, etc..
 
-Clean the dataset, convert string to numeric, filling null, etc..
+## 3. Variable selection
 
-### 3. Variable selection
+    Find the most optimal variable sets.
 
-Find the most optimal variable sets.
+## 4. Algorithm selection
 
-### 4. Try with different ML algorithm and tweak hyper parameters
+    Try with different ML algorithms and tweak hyper parameters
 
-RandomForestClassification
-![](./docImages/tree_small.png)
+| Algorithm                  | Description                     |
+| -------------------------- | ------------------------------- |
+| RandomForestClassification | ![](./docImages/tree_small.png) |
+| Neural Network             | ![](./docImages/nn_small.png)   |
 
-ANN Dense
-![](./docImages/nn_small.png)
+    Choose the best performance
+    Neural Network (NN) was chosen for this applicaiton.
 
 ![](./docImages/ml_model_selection.png)
 
-### Final model's confusion matrix
+## 5. Confusion matrix from NN model
 
 ![](./docImages/confusion_matrix.png)
 
-- It was difficult to model it since a huge factor of popularity is the name of artist.
-- Misclassifcation between unpopular(Label 0) and unpopular(Label 2) is very rare.
-- This model can provide a good indicator of popularity even without the name of artists.
+    a. It was not easy to model it since there are many features in the dataset, and unusable variable of the artists was pretty signification variable for modeling.
+    b. Misclassifcation between unpopular(Label 0) and unpopular(Label 2) is very rare (Very good!).
+    c. This model can provide a good indicator of popularity even without the name of artists.
 
-### Model and scaler are stored as "Trained Model".
+## 6. Model and scaler are stored as "Trained Model".
 
 ![](./docImages/production_pipeline.png)
 
 ## Conclusion
 
-### Creating a model takes a lot of effort. Feature engineering part is extremely import to start with optimal dataset. Any redundant variables yield sideeffect (cost of CPU, memory, efficiency etc..).
+### 1. Creating a model takes a lot of effort. Feature engineering part is extremely important which allows to start with optimal dataset. Any redundant variables yield side effect (high cost of CPU, big memory usage, low accuracy etc..).
 
-### Trying different ML algorithms and consider computing power and memroy required for the target productoin server. 
+### 2. Trying different ML algorithms and consider computing power and memroy required for the target productoin server.
 
-Source of images: TBD
+Source of images: https://www.trainindata.com/
+
 
 `;
 
